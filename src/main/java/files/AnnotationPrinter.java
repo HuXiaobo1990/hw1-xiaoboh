6547843282f21834db705ef019c2c6384a9f3262
xiaobo_hu <carlos.hxb@gmail.com>
2012-12-03 00:11:00 -0500

    0.0.9

--- src/main/java/files/AnnotationPrinter.java
+++ src/main/java/files/AnnotationPrinter.java
@@ -126,17 +126,6 @@ public class AnnotationPrinter extends CasConsumer_ImplBase implements CasObject
     Iterator annotationIter = jcas.getIndexRepository().getIndex("genetagIndex").iterator();
     while (annotationIter.hasNext()) {
       genetag annot = (genetag) annotationIter.next();
-      if (titleP == false) {
-        try {
-          fileWriter.write("\n\n<++++NEW DOCUMENT++++>\n");
-          if (docUri != null)
-            fileWriter.write("DOCUMENT URI:" + docUri + "\n");
-          fileWriter.write("\n");
-        } catch (IOException e) {
-          throw new ResourceProcessException(e);
-        }
-        titleP = true;
-      }
       // get the text that is enclosed within the annotation in the CAS
       String aText = annot.getCoveredText();
       aText = aText.replace('\n', ' ');
